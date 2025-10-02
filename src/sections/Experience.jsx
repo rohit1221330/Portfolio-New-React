0a0f57f14d641c55c57df0d1ab52cd5b03eb5986
Rohit Dhyani <rohitdhyani50@gmail.com>
2025-10-02 22:18:18 +0530

    Fixed experience section error

--- src/sections/Experience.jsx
+++ src/sections/Experience.jsx
@@ -2,7 +2,7 @@
 
 import React, { useState } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
-import { experiences } from '../constants/index';
+import { experiences } from '../constants/Index';
 
 const Experience = () => {
   const [selectedJob, setSelectedJob] = useState(experiences[0]);
