21f41505e7740e15f4203cb425c0092350939f5f
Rohit Dhyani <rohitdhyani50@gmail.com>
2025-10-02 22:15:35 +0530

    Fixed navbar Error

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@
 
 import React, { useState, useEffect, useRef } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
-import { navLinks } from '../constants';
+import { navLinks } from '../constants/Index.jsx';
 import { FiMenu, FiX } from 'react-icons/fi';
 
 const Navbar = () => {
